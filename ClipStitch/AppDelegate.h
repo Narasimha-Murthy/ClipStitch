793bec76efc2273fca00cbc130f48e8d2cf39f7c
iApp <iapp@iApps-iMac.local>
2015-10-07 16:41:32 +0530

    Initial Commit

---
+++ ClipStitch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClipStitch
+//
+//  Created by iApp on 07/10/15.
+//  Copyright (c) 2015 iApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClipStitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClipStitch
+//
+//  Created by iApp on 07/10/15.
+//  Copyright (c) 2015 iApp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
